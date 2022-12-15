8de469dd500dbe9e7d91838b56049743ec49d1c7
Your Name <you@example.com>
2022-12-15 17:16:04 +0330

    clear commented

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,11 @@
+// let count = prompt("enter Number of Cars:");
 // function CarCreator(cName){
 //     this.carName= cName;
 //     this.position=0;
 //     this.order= Math.random();
 // };
 // let cars=[];
-// for(let i=0; i<5;i++){
+// for(let i=0; i<count;i++){
 //     let userName= prompt("enter a Name: ");
 //     let car = new CarCreator(userName);
 //     cars.push(car);
--- JS/methods.js
+++ JS/methods.js
@@ -1,35 +1,46 @@
-let numberOfCars = parseInt(prompt("Enter number of cars: "));
-let cars = [];
-for (let i = 0; i < numberOfCars; i++) {
-  let x;
-  switch ((i + 1) % 10) {
-    case 1:
-      x = "'st";
-      break;
-    case 2:
-      x = "'nd";
-      break;
-    case 3:
-      x = "'rd";
-      break;
-    default:
-      x = "'th";
-  }
-  if (i + 1 === 11 || i + 1 === 12 || i + 1 === 13) {
-    x = "'th";
-  }
-
-  let cName = prompt("please enter name of the " + (i + 1) + x + " car");
-
-  function CarCreator(cName) {
-    this.carName = cName;
-    this.position = 0;
-    this.order= Math.random();
-  }
-  let raceCAr = new CarCreator(cName);
-  cars.push(raceCAr);
+// let numberOfCars = parseInt(prompt("Enter number of cars: "));
+// let cars = [];
+// function CarCreator(cName) {
+//   this.carName = cName;
+//   this.position = 0;
+//   this.order= Math.random();
+// }
+// for (let i = 0; i < numberOfCars; i++) {
+//   let x;
+//   switch ((i + 1) % 10) {
+//     case 1:
+//       x = "'st";
+//       break;
+//     case 2:
+//       x = "'nd";
+//       break;
+//     case 3:
+//       x = "'rd";
+//       break;
+//     default:
+//       x = "'th";
+//   }
+//   if (i + 1 === 11 || i + 1 === 12 || i + 1 === 13) {
+//     x = "'th";
+//   }
+let cars=[];
+let numberOfCars= parseInt(prompt("Enter Number of cars: "));
+function CarCreator(cName){
+  this.carName= cName;
+  this.position=0;
+  this.order= Math.random();
+}
+for(let i=0;i<numberOfCars;i++){
+  let carName= prompt("please choose a name for car Numer "+ (i+1));
+  let raceCar = new CarCreator(carName);
+  cars.push(raceCar);
 }
 console.log(cars);
+//   let userName = prompt("please enter name of the " + (i + 1) + x + " car");
+//   let raceCAr = new CarCreator(userName);
+//   cars.push(raceCAr);
+// }
+// console.log(cars);
 
 function replacechar(str, index, char) {
   return str.slice(0, index) + char + str.slice(index + 1);
