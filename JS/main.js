8e11eb58e4942cc58ef865adb3f0fd98e782b43d
Your Name <you@example.com>
2022-12-15 13:52:43 +0330

    pre cars problem fix

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
+// function CarCreator(cName){
+//     this.carName= cName;
+//     this.position=0;
+//     this.order= Math.random();
+// };
+// let cars=[];
+// for(let i=0; i<5;i++){
+//     let userName= prompt("enter a Name: ");
+//     let car = new CarCreator(userName);
+//     cars.push(car);
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// console.log(cars);
--- JS/methods.js
+++ JS/methods.js
@@ -1,65 +1,81 @@
-// let numberOfCars = parseInt(prompt("Enter number of cars: "));
-// let cars = [];
-// for (let i = 0; i < numberOfCars; i++) {
-//   let x;
-//   switch ((i + 1) % 10) {
-//     case 1:
-//       x = "'st";
-//       break;
-//     case 2:
-//       x = "'nd";
-//       break;
-//     case 3:
-//       x = "'rd";
-//       break;
-//     default:
-//       x = "'th";
-//   }
-//   if (i + 1 === 11 || i + 1 === 12 || i + 1 === 13) {
-//     x = "'th";
-//   }
+let numberOfCars = parseInt(prompt("Enter number of cars: "));
+let cars = [];
+for (let i = 0; i < numberOfCars; i++) {
+  let x;
+  switch ((i + 1) % 10) {
+    case 1:
+      x = "'st";
+      break;
+    case 2:
+      x = "'nd";
+      break;
+    case 3:
+      x = "'rd";
+      break;
+    default:
+      x = "'th";
+  }
+  if (i + 1 === 11 || i + 1 === 12 || i + 1 === 13) {
+    x = "'th";
+  }
 
-//   let cName = prompt("please enter name of the " + (i + 1) + x + " car");
+  let cName = prompt("please enter name of the " + (i + 1) + x + " car");
 
-//   function CarCreator(cName) {
-//     this.carName = cName;
-//     this.position = 0;
-//     this.rank = 0;
-//   }
-//   let raceCAr = new CarCreator(cName);
-//   cars.push(raceCAr);
-// }
+  function CarCreator(cName) {
+    this.carName = cName;
+    this.position = 0;
+    this.order= Math.random();
+  }
+  let raceCAr = new CarCreator(cName);
+  cars.push(raceCAr);
+}
+console.log(cars);
 
-// function replacechar(str, index, char) {
-//   return str.slice(0, index) + char + str.slice(index + 1);
-// }
-// let map = [];
-// for (let i = 0; i < 300; i++) {
-//   map.push("*");
-// }
-
-// console.log(cars);
-// map = map.join("");
-// let originalMap= map;
-let hitCar;
-// for (let i = 0; i < 5; i++) {
-//   let positionArray = [];
-//   map=originalMap;
-//   for (item of cars) {
-//     let distance = parseInt(Math.random() * 10 + 1);
-replacechar(map,item.position, "*");
-//     item.position += distance;
-if(map[item.position] !== "*"){
- hitCar= map[item.position];
+function replacechar(str, index, char) {
+  return str.slice(0, index) + char + str.slice(index + 1);
+}
+let map = [];
+for (let i = 0; i < 300; i++) {
+  map.push("*");
 }
 
+map = map.join("");
+let originalMap = map;
+let finished = [];
+while(finished.length<numberOfCars){
+  let moveArray = [];
+  let positionArray = [];
+   for (item of cars) {
+  
 
-//     positionArray.push(item.position);
-//     map = replacechar(map, item.position, item.carName);
-    
-//   }
-//   console.log(positionArray);
-//   console.log(map);
+    let distance = parseInt(Math.random() * 10 + 1);
+    moveArray.push(distance);
+    map = replacechar(map, item.position, "*");
+    item.position += distance;
+    if (item.position >= 299 && !finished.includes(item.carName)) {
+      finished.push(item.carName);
+      cars.splice(cars.indexOf(item),1);
+      map=map.slice(0,299)+"*";
+    }
+    else{
+      if (map[item.position] !== "*" && item.position !==0 ) {
+        let hitCar = map[item.position];
+        for (element of cars) {
+          if (element.carName === hitCar) {
+            element.position = 0;
+          }
+        }
+      }
+  
+      positionArray.push(item.position);
+      map = replacechar(map, item.position, item.carName);
+    }
+  }
+  console.log(map);
+  console.log("------------------------------------------");
+}
+console.log(finished);
+console.log("winner: "+ finished[0]);
 
-// }
+
 
